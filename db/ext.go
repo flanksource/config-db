3536dce07737ac8acc6529bdb604cdbb63dbd35a
Moshe Immerman <moshe@flanksource.com>
2022-08-21 16:12:43 +0000

    chore: fix build error

--- db/ext.go
+++ db/ext.go
@@ -8,7 +8,6 @@ import (
 	"github.com/flanksource/confighub/db/models"
 )
 
-// GetJSON ...
 func GetJSON(ci models.ConfigItem) []byte {
 	data, err := json.Marshal(ci.Config)
 	if err != nil {
@@ -17,27 +16,22 @@ func GetJSON(ci models.ConfigItem) []byte {
 	return data
 }
 
-// GetConfigItem ...
-func GetConfigItem(id string) (*models.ConfigItem, error) {
-	return repository.GetConfigItem(id)
+func GetConfigItem(externalType, id string) (*models.ConfigItem, error) {
+	return repository.GetConfigItem(externalType, id)
 }
 
-// CreateConfigItem ...
 func CreateConfigItem(item *models.ConfigItem) error {
 	return repository.CreateConfigItem(item)
 }
 
-// UpdateConfigItem ...
 func UpdateConfigItem(item *models.ConfigItem) error {
 	return repository.UpdateConfigItem(item)
 }
 
-// CreateConfigChange ...
 func CreateConfigChange(change *models.ConfigChange) error {
 	return repository.CreateConfigChange(change)
 }
 
-// QueryConfigItems ...
 func QueryConfigItems(request v1.QueryRequest) (*v1.QueryResult, error) {
 	return repository.QueryConfigItems(request)
 }
