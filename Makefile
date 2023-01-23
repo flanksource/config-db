339e73f6a3e9c2f78f6bc76c4f8bffd77bd27183
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-01-23 12:17:16 +0530

    chore: move cron code to a different file

--- Makefile
+++ Makefile
@@ -30,7 +30,7 @@ docker-push:
 manifests: controller-gen ## Generate WebhookConfiguration, ClusterRole and CustomResourceDefinition objects.
 	$(CONTROLLER_GEN) rbac:roleName=manager-role crd webhook paths="./..." output:crd:artifacts:config=config/crd/bases
 	# TODO: Might not need webhook and rbac to regen always
-	$(CONTROLLER_GEN) crd paths="./api/..." output:stdout > config/deploy/crd-todo.yaml
+	$(CONTROLLER_GEN) crd paths="./api/..." output:stdout > config/crd/crd-todo.yaml
 
 .PHONY: generate
 generate: controller-gen ## Generate code containing DeepCopy, DeepCopyInto, and DeepCopyObject method implementations.
--- scrapers/run.go
+++ scrapers/run.go
@@ -4,16 +4,10 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/flanksource/commons/logger"
 	v1 "github.com/flanksource/config-db/api/v1"
 	"github.com/flanksource/config-db/db"
 	"github.com/flanksource/config-db/utils/kube"
-	"github.com/robfig/cron/v3"
 )
-
-var cronManger *cron.Cron
-var DefaultSchedule string
-var cronIDFunctionMap map[string]cron.EntryID
 
 func RunScraper(scraper v1.ConfigScraper) error {
 	kommonsClient, err := kube.NewKommonsClient()
@@ -31,38 +25,4 @@ func RunScraper(scraper v1.ConfigScraper) error {
 		return fmt.Errorf("Failed to update db: %v", err)
 	}
 	return nil
-}
-
-func AddToCron(scraper v1.ConfigScraper, id string) {
-	fn := func() {
-		if err := RunScraper(scraper); err != nil {
-			logger.Errorf("Error running scraper: %v", err)
-		}
-	}
-	schedule := scraper.Schedule
-	if schedule == "" {
-		schedule = DefaultSchedule
-	}
-
-	// If id already exists, remove the old cron job
-	if entryID, exists := cronIDFunctionMap[id]; exists {
-		cronManger.Remove(entryID)
-	}
-	// Schedule a new job
-	entryID, err := cronManger.AddFunc(schedule, fn)
-	if err != nil {
-		logger.Errorf("Failed to schedule cron using :%v", err)
-		return
-	}
-
-	// Add non empty ids to the map
-	if id != "" {
-		cronIDFunctionMap[id] = entryID
-	}
-}
-
-func init() {
-	cronIDFunctionMap = make(map[string]cron.EntryID)
-	cronManger = cron.New()
-	cronManger.Start()
 }
