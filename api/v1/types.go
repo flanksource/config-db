927525384251d89162896c954b3199d9b8edd020
basebandit <evansonmwangi83@gmail.com>
2023-02-04 16:51:44 +0300

    chore: add github actions config scraper

--- api/v1/types.go
+++ api/v1/types.go
@@ -18,6 +18,7 @@ type ConfigScraper struct {
 	Kubernetes     []Kubernetes     `json:"kubernetes,omitempty" yaml:"kubernetes,omitempty"`
 	KubernetesFile []KubernetesFile `json:"kubernetesFile,omitempty" yaml:"kubernetesFile,omitempty"`
 	AzureDevops    []AzureDevops    `json:"azureDevops,omitempty" yaml:"azureDevops,omitempty"`
+	GithubActions  []GitHubActions  `json:"githubActions,omitempty" yaml:"githubActions,omitempty"`
 	SQL            []SQL            `json:"sql,omitempty" yaml:"sql,omitempty"`
 }
 
