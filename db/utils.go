89ee8ed356f8601dd87ef8217496f2d01b24a4b4
Aditya <thebeaditya@gmail.com>
2023-03-02 13:58:07 +0545

    chore: added documentation to IsUniqueConstraintPGErr()

--- db/utils.go
+++ db/utils.go
@@ -10,6 +10,10 @@ const (
 	pgUniqueViolationErrCode = "23505" // https://hackage.haskell.org/package/postgresql-error-codes-1.0.1/docs/PostgreSQL-ErrorCodes.html#t:ErrorCode
 )
 
+// IsUniqueConstraintPGErr identifies whether the error is a postgres unique constraint error.
+//
+// gorm doesn't have a native way to identify this error
+// https://github.com/go-gorm/gorm/issues/4037
 func IsUniqueConstraintPGErr(err error) bool {
 	pgErr := new(pgconn.PgError)
 	return errors.As(err, &pgErr) && pgErr.Code == pgUniqueViolationErrCode
