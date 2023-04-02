398a82886999b0677ad3bad579fe601b5fe4b035
Moshe Immerman <moshe@flanksource.com>
2023-04-02 13:44:21 +0000

    chore: switch to upstream mssql driver

--- scrapers/sql/sql.go
+++ scrapers/sql/sql.go
@@ -8,9 +8,9 @@ import (
 	"github.com/xo/dburl"
 
 	//drivers
-	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
+	_ "github.com/microsoft/go-mssqldb"
 )
 
 type SqlScraper struct {
