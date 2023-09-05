0c1e43b88217334dd6bd7ec7d0265144d21c2bec
Aditya Thebe <thebeaditya@gmail.com>
2023-09-05 20:58:46 +0545

    chore: update getSourceFromEvent test

--- scrapers/kubernetes/events_test.go
+++ scrapers/kubernetes/events_test.go
@@ -2,56 +2,33 @@ package kubernetes
 
 import (
 	"testing"
-
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 func Test_getSourceFromEvent(t *testing.T) {
-	type args struct {
-		obj *unstructured.Unstructured
-	}
 	tests := []struct {
 		name string
-		args args
+		args Event
 		want string
 	}{
 		{
-			name: "simple", args: args{
-				&unstructured.Unstructured{
-					Object: map[string]interface{}{
-						"source": map[string]interface{}{
-							"component": "kubelet",
-							"host":      "minikube",
-						},
-					},
+			name: "simple", args: Event{
+				Source: map[string]interface{}{
+					"component": "kubelet",
+					"host":      "minikube",
 				},
 			},
 			want: "kubernetes/component=kubelet,host=minikube",
 		},
 		{
-			name: "empty", args: args{
-				&unstructured.Unstructured{
-					Object: map[string]interface{}{
-						"source": map[string]interface{}{},
-					},
-				},
+			name: "empty", args: Event{
+				Source: map[string]any{},
 			},
 			want: "kubernetes/",
-		},
-		{
-			name: "nil source", args: args{
-				&unstructured.Unstructured{
-					Object: map[string]interface{}{
-						"source": nil,
-					},
-				},
-			},
-			want: "",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getSourceFromEvent(tt.args.obj); got != tt.want {
+			if got := getSourceFromEvent(tt.args); got != tt.want {
 				t.Errorf("getSourceFromEvent() = %v, want %v", got, tt.want)
 			}
 		})
