a0e238f2522756b896019fefd466e02aeb48c84e
Aditya Thebe <thebeaditya@gmail.com>
2024-01-31 15:58:26 +0545

    feat: make config.properties use filter (#375)
    * feat: make config.properties use filter
    
    * chore: just copy the links of the property

--- scrapers/processors/json.go
+++ scrapers/processors/json.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/flanksource/commons/logger"
 	v1 "github.com/flanksource/config-db/api/v1"
-	"github.com/flanksource/config-db/utils"
+	"github.com/flanksource/duty/types"
 	"github.com/flanksource/gomplate/v3"
 	"github.com/magiconair/properties"
 	"github.com/ohler55/ojg/jp"
@@ -188,7 +188,23 @@ func (e Extract) Extract(inputs ...v1.ScrapeResult) ([]v1.ScrapeResult, error) {
 			}
 		}
 
-		if properties, ok := input.BaseScraper.Properties[input.Type]; ok {
+		for i, configProperty := range input.BaseScraper.Properties {
+			if configProperty.Filter != "" {
+				if response, err := gomplate.RunTemplate(input.AsMap(), gomplate.Template{Expression: configProperty.Filter}); err != nil {
+					input.Errorf("failed to parse filter: %v", err)
+					continue
+				} else if boolVal, err := strconv.ParseBool(response); err != nil {
+					input.Errorf("expected a boolean but property filter returned (%s)", response)
+					continue
+				} else if !boolVal {
+					continue
+				}
+			}
+
+			// clone the links so as to not mutate the original Links template
+			configProperty.Links = make([]types.Link, len(input.BaseScraper.Properties[i].Links))
+			copy(configProperty.Links, input.BaseScraper.Properties[i].Links)
+
 			templater := gomplate.StructTemplater{
 				Values:         input.AsMap(),
 				ValueFunctions: true,
@@ -198,16 +214,12 @@ func (e Extract) Extract(inputs ...v1.ScrapeResult) ([]v1.ScrapeResult, error) {
 				},
 			}
 
-			// Need to perform a deep clone otherwise this will affect the base scraper
-			// and hence all the inputs that come after.
-			input.Properties, err = utils.CloneWithJSON(properties)
-			if err != nil {
-				return results, fmt.Errorf("failed to clone config properties: %w", err)
+			if err := templater.Walk(configProperty); err != nil {
+				input.Errorf("failed to template scraper properties: %v", err)
+				continue
 			}
 
-			if err := templater.Walk(input.Properties); err != nil {
-				return results, fmt.Errorf("failed to template scraper properties: %w", err)
-			}
+			input.Properties = append(input.Properties, &configProperty.Property)
 		}
 
 		if input.Format == "properties" {
