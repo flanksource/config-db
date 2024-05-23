000d49880afaf4cd78e52a9625febc802f049137
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-05-23 15:58:54 +0530

    chore: convert policy doc to json

--- utils/json.go
+++ utils/json.go
@@ -21,9 +21,19 @@ func StructToJSON(v any) (string, error) {
 // ToJSONMap takes an input value of struct or map type and converts it to a map[string]any representation
 // using JSON encoding and decoding.
 func ToJSONMap(s any) (map[string]any, error) {
-	raw, err := json.Marshal(s)
-	if err != nil {
-		return nil, err
+	var raw []byte
+	var err error
+
+	switch s.(type) {
+	case string:
+		raw = []byte(s.(string))
+	case []byte:
+		raw = s.([]byte)
+	default:
+		raw, err = json.Marshal(s)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	result := make(map[string]any)
