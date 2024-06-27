0147100fa716a2a7ff582c3204f8585f40c2158a
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-06-27 10:55:01 +0530

    chore: cleanup config_items using deferred constraints

--- jobs/cleanup.go
+++ jobs/cleanup.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/flanksource/duty"
 	"github.com/flanksource/duty/job"
 )
 
@@ -74,82 +75,29 @@ var CleanupConfigChanges = &job.Job{
 
 var CleanupConfigItems = &job.Job{
 	Name:       "CleanupConfigItems",
-	Schedule:   "@every 24h",
+	Schedule:   "0 2 * * *", // Everynight at 2 AM
 	Singleton:  true,
 	JobHistory: true,
 	Retention:  job.RetentionBalanced,
 	Fn: func(ctx job.JobRuntime) error {
 		ctx.History.ResourceType = JobResourceType
 		retention := ctx.Properties().Duration("config.retention.period", (time.Hour * 24 * time.Duration(ConfigItemRetentionDays)))
-		seconds := int64(retention.Seconds())
+		days := int64(retention.Hours() / 24)
 
-		ctx.Tracef("cleaning up config items older than %v", retention)
-
-		linkedConfigsQuery := `
-		SELECT config_id FROM evidences WHERE config_id IS NOT NULL
-		UNION
-		SELECT config_id FROM config_changes WHERE id IN (SELECT config_change_id FROM evidences)
-		UNION
-		SELECT config_id FROM config_analysis WHERE id IN (SELECT config_analysis_id FROM evidences)
-		UNION
-		SELECT config_id FROM playbook_RUNS WHERE config_id IS NOT NULL
-		`
-
-		relationshipDeleteQuery := fmt.Sprintf(`
-		DELETE FROM config_relationships
-		WHERE deleted_at < NOW() - interval '1 SECONDS' * ?
-		OR config_id in (SELECT id FROM config_items WHERE id NOT IN (%s) AND deleted_at < NOW() - interval '1 SECONDS' * ?)
-		OR related_id in (SELECT id FROM config_items WHERE id NOT IN (%s) AND deleted_at < NOW() - interval '1 SECONDS' * ?)`, linkedConfigsQuery, linkedConfigsQuery)
-		if tx := ctx.Context.DB().Exec(relationshipDeleteQuery, seconds, seconds, seconds); tx.Error != nil {
-			return fmt.Errorf("failed to delete config relationships: %w", tx.Error)
-		} else {
-			ctx.Tracef("deleted %d config relationships", tx.RowsAffected)
+		var prevCount int64
+		if err := ctx.DB().Raw("SELECT COUNT(*) FROM config_items").Scan(&prevCount).Error; err != nil {
+			return fmt.Errorf("failed to count config items: %w", duty.DBErrorDetails(err))
 		}
 
-		// break the parent relationship of deleted configs
-		breakParentRelationshipQuery := fmt.Sprintf(`
-		UPDATE config_items
-		SET parent_id = NULL
-		WHERE
-            id NOT IN (%s) AND
-            parent_id IS NOT NULL AND
-            deleted_at < NOW() - interval '1 SECONDS' * ?`,
-			linkedConfigsQuery)
-		if tx := ctx.Context.DB().Exec(breakParentRelationshipQuery, seconds); tx.Error != nil {
-			return fmt.Errorf("failed to remove config parent relationships: %w", tx.Error)
-		} else {
-			ctx.Tracef("removed %d config parent relationships", tx.RowsAffected)
+		if err := ctx.DB().Exec("SELECT delete_old_config_items(?)", days).Error; err != nil {
+			return fmt.Errorf("failed to delete config items: %w", duty.DBErrorDetails(err))
 		}
 
-		var iter int
-		deleteBatchSize := ctx.Properties().Int("config.retention.delete_batch_size", 500)
-		configDeleteQuery := fmt.Sprintf(`
-		WITH ordered_rows AS (
-			SELECT id
-			FROM config_items
-			WHERE
-				deleted_at < NOW() - interval '1 SECONDS' * ? AND
-				id NOT IN (%s)
-			ORDER BY length(path) DESC
-			LIMIT ?
-		)
-		DELETE FROM config_items
-		WHERE id IN (SELECT id FROM ordered_rows)`, linkedConfigsQuery)
-		for {
-			iter++
-			tx := ctx.Context.DB().Exec(configDeleteQuery, seconds, deleteBatchSize)
-			if tx.Error != nil {
-				return fmt.Errorf("failed to delete config items: %w", tx.Error)
-			}
-
-			if tx.RowsAffected == 0 {
-				break
-			}
-
-			ctx.Logger.V(2).Infof("hard deleted %d config items [iter=%d, batchsize=%d]", iter, deleteBatchSize, tx.RowsAffected)
-			ctx.History.SuccessCount += int(tx.RowsAffected)
+		var newCount int64
+		if err := ctx.DB().Raw("SELECT COUNT(*) FROM config_items").Scan(&newCount).Error; err != nil {
+			return fmt.Errorf("failed to count config items: %w", duty.DBErrorDetails(err))
 		}
-
+		ctx.History.SuccessCount = int(newCount - prevCount)
 		return nil
 	},
 }
