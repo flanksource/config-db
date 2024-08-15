51611c18190a0071e881025b6f701abc364a082a
Aditya Thebe <contact@adityathebe.com>
2024-08-15 14:39:14 +0545

    fix: duplicate key value violates unique constraint "config_changes_config_id_external_change_id_key"

--- db/models/config_change.go
+++ db/models/config_change.go
@@ -76,6 +76,13 @@ func (c *ConfigChange) BeforeCreate(tx *gorm.DB) (err error) {
 		c.ID = uuid.New().String()
 	}
 
-	tx.Statement.AddClause(clause.OnConflict{UpdateAll: true})
+	tx.Statement.AddClause(clause.OnConflict{
+		Columns: []clause.Column{
+			{Name: "config_id"},
+			{Name: "external_change_id"},
+		},
+		DoNothing: true,
+	})
+
 	return
 }
