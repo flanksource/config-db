19a80d0f4cd52c87d49e6867a6366caa7e373076
Yash Mehrotra <yashmehrotra95@gmail.com>
2025-02-12 12:10:14 +0530

    refactor: use flat map for resource id map

--- scrapers/kubernetes/resource_map.go
+++ scrapers/kubernetes/resource_map.go
@@ -1,45 +1,34 @@
 package kubernetes
 
 import (
+	"fmt"
 	"sync"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
-// map<namespace><kind><name>: <id>
-type ResourceIDMap map[string]map[string]map[string]string
+type ResourceIDMap map[string]string
 
 type ResourceIDMapContainer struct {
 	mu   sync.RWMutex
 	data ResourceIDMap
 }
 
+func ResourceIDMapKey(namespace, kind, name string) string {
+	return fmt.Sprintf("namespace=%s,kind=%s,name=%s", namespace, kind, name)
+}
+
 func (t *ResourceIDMapContainer) Set(namespace, kind, name, id string) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
-	if t.data[namespace] == nil {
-		t.data[namespace] = make(map[string]map[string]string)
-	}
-	if t.data[namespace][kind] == nil {
-		t.data[namespace][kind] = make(map[string]string)
-	}
-	t.data[namespace][kind][name] = id
+	t.data[ResourceIDMapKey(namespace, kind, name)] = id
 }
 
 func (t *ResourceIDMapContainer) Get(namespace, kind, name string) string {
 	t.mu.RLock()
 	defer t.mu.RUnlock()
-
-	if kinds, ok := t.data[namespace]; ok {
-		if names, ok := kinds[kind]; ok {
-			if id, ok := names[name]; ok {
-				return id
-			}
-		}
-	}
-
-	return ""
+	return t.data[ResourceIDMapKey(namespace, kind, name)]
 }
 
 type PerClusterResourceIDMap struct {
@@ -76,15 +65,9 @@ func (t *PerClusterResourceIDMap) MergeAndUpdate(clusterID string, resourceIDMap
 }
 
 func NewResourceIDMap(objs []*unstructured.Unstructured) *ResourceIDMapContainer {
-	resourceIDMap := make(map[string]map[string]map[string]string)
+	resourceIDMap := make(ResourceIDMap)
 	for _, obj := range objs {
-		if resourceIDMap[obj.GetNamespace()] == nil {
-			resourceIDMap[obj.GetNamespace()] = make(map[string]map[string]string)
-		}
-		if resourceIDMap[obj.GetNamespace()][obj.GetKind()] == nil {
-			resourceIDMap[obj.GetNamespace()][obj.GetKind()] = make(map[string]string)
-		}
-		resourceIDMap[obj.GetNamespace()][obj.GetKind()][obj.GetName()] = string(obj.GetUID())
+		resourceIDMap[ResourceIDMapKey(obj.GetNamespace(), obj.GetKind(), obj.GetName())] = string(obj.GetUID())
 	}
 
 	return &ResourceIDMapContainer{
@@ -106,28 +89,12 @@ func mergeResourceIDMap(latest, cached ResourceIDMap) ResourceIDMap {
 
 	// First, copy all data from cached
 	for k, v := range cached {
-		output[k] = make(map[string]map[string]string)
-		for k2, v2 := range v {
-			output[k][k2] = make(map[string]string)
-			for k3, v3 := range v2 {
-				output[k][k2][k3] = v3
-			}
-		}
+		output[k] = v
 	}
 
 	// Then, update or add data from latest
 	for k, v := range latest {
-		if _, ok := output[k]; !ok {
-			output[k] = make(map[string]map[string]string)
-		}
-		for k2, v2 := range v {
-			if _, ok := output[k][k2]; !ok {
-				output[k][k2] = make(map[string]string)
-			}
-			for k3, v3 := range v2 {
-				output[k][k2][k3] = v3
-			}
-		}
+		output[k] = v
 	}
 
 	return output
