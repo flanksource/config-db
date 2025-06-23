21cd10a6bb68a5abede155df04268e05cf0e72fd
Yash Mehrotra <yashmehrotra95@gmail.com>
2025-06-23 11:25:21 +0530

    chore: address review comments

--- api/v1/interface.go
+++ api/v1/interface.go
@@ -455,15 +455,10 @@ func (s *ScrapeResults) Analysis(analyzer string, configType string, id string) 
 }
 
 func (s *ScrapeResults) Errorf(e error, msg string, args ...any) ScrapeResults {
-	logger.Errorf("%s: %v", fmt.Sprintf(msg, args...), e)
-	*s = append(*s, ScrapeResult{Error: e})
-	return *s
-}
-
-func ScrapeResultErrorf(e error, msg string, args ...any) ScrapeResult {
 	errMsg := fmt.Sprintf(msg, args...)
 	logger.Errorf("%s: %v", errMsg, e)
-	return ScrapeResult{Error: fmt.Errorf("%s: %w", errMsg, e)}
+	*s = append(*s, ScrapeResult{Error: fmt.Errorf("%s: %w", errMsg, e)})
+	return *s
 }
 
 type Tag struct {
