75f9ba0daf0a552b43ccc5074fb7438bebf00db0
Aditya Thebe <contact@adityathebe.com>
2026-01-05 20:19:20 +0545

    feat: scraper run now detached from HTTP request context but still waits (#1826)
    * feat: scraper run now async
    
    * wait for the run
    
    * return error

--- scrapers/run_now.go
+++ scrapers/run_now.go
@@ -3,26 +3,27 @@ package scrapers
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	dutyAPI "github.com/flanksource/duty/api"
+	"github.com/flanksource/duty/context"
+	"github.com/labstack/echo/v4"
 
 	"github.com/flanksource/config-db/api"
 	v1 "github.com/flanksource/config-db/api/v1"
 	"github.com/flanksource/config-db/db"
-	"github.com/flanksource/duty/context"
-	"github.com/labstack/echo/v4"
 )
+
+const runNowTimeout = 30 * time.Minute
 
 func RunNowHandler(c echo.Context) error {
 	id := c.Param("id")
+	baseCtx := c.Request().Context().(context.Context)
 
-	baseCtx := c.Request().Context()
-	ctx := baseCtx.(context.Context)
-
-	scraper, err := db.FindScraper(ctx, id)
+	scraper, err := db.FindScraper(baseCtx, id)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error()) // could mean server errors as well, but there's no trivial way to find out...
-	}
-
-	if scraper == nil {
+	} else if scraper == nil {
 		return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("scraper with id=%s was not found", id))
 	}
 
@@ -31,10 +32,38 @@ func RunNowHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to transform config scraper model", err)
 	}
 
-	scrapeCtx := api.NewScrapeContext(ctx).WithScrapeConfig(&configScraper)
-	j := newScraperJob(scrapeCtx)
-	j.JitterDisable = true
-	j.Run()
+	resultCh := make(chan error, 1)
+	go func() {
+		defer close(resultCh)
 
-	return c.JSON(http.StatusOK, j.LastJob.Details)
+		ctx, cancel := context.New().
+			WithDB(baseCtx.DB(), baseCtx.Pool()).
+			WithSubject(baseCtx.Subject()).
+			WithTimeout(runNowTimeout)
+		defer cancel()
+
+		scrapeCtx := api.NewScrapeContext(ctx).WithScrapeConfig(&configScraper)
+		j := newScraperJob(scrapeCtx)
+		j.JitterDisable = true
+		j.Run()
+
+		var runErr error
+		if j.LastJob == nil {
+			runErr = fmt.Errorf("scraper run completed without job history")
+		} else {
+			runErr = j.LastJob.AsError()
+		}
+		resultCh <- runErr
+	}()
+
+	select {
+	case err := <-resultCh:
+		if err != nil {
+			return dutyAPI.WriteError(c, err)
+		}
+		return dutyAPI.WriteSuccess(c, nil)
+
+	case <-c.Request().Context().Done():
+		return c.Request().Context().Err()
+	}
 }
