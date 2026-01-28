bf2615aed8d076f39e74c786a16a327ad755a0e9
Moshe Immerman <moshe@flanksource.com>
2026-01-28 14:49:20 +0200

    feat: add change redirect support and processor improvements
    - Add ConfigID, ConfigType, ScraperID fields to ChangeMapping for redirecting changes
    - Add Pretty() methods for Script and Connection types
    - Add IsDebug() and IsTrace() methods to ScrapeConfig
    - Support config_id, config_type, scraper_id in change rules for redirection
    - Improve JSON processor with Tags/Labels JSONPath extraction and better error messages
    - Add debug logging for script output parsing
    - Add debug/trace logging with clicky formatting for SQL scraper

--- api/v1/http.go
+++ api/v1/http.go
@@ -9,8 +9,7 @@ type HTTP struct {
 	BaseScraper               `json:",inline"`
 	connection.HTTPConnection `json:",inline"`
 	// Environment variables to be used in the templating.
-	Env     []types.EnvVar          `json:"env,omitempty"`
-	Method  *string                 `json:"method,omitempty"`
-	Body    *string                 `json:"body,omitempty"`
-	Headers map[string]types.EnvVar `json:"headers,omitempty"`
+	Env    []types.EnvVar `json:"env,omitempty"`
+	Method *string        `json:"method,omitempty"`
+	Body   *string        `json:"body,omitempty"`
 }
