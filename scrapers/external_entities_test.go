6f8841df2f097e3327c8f15fbf593a83f958a3e3
Aditya Thebe <contact@adityathebe.com>
2026-04-20 21:40:11 +0545

    fix: entities test

--- scrapers/external_entities_test.go
+++ scrapers/external_entities_test.go
@@ -169,6 +169,9 @@ var _ = Describe("Stale external entities deletion test", Ordered, func() {
 		err = DefaultContext.DB().Unscoped().Where("scraper_id = ?", scraperModel.ID).Delete(&dutymodels.ExternalUser{}).Error
 		Expect(err).NotTo(HaveOccurred(), "failed to delete external users")
 
+		err = DefaultContext.DB().Unscoped().Where("scraper_id = ?", scraperModel.ID).Delete(&dutymodels.ExternalRole{}).Error
+		Expect(err).NotTo(HaveOccurred(), "failed to delete external roles")
+
 		err = DefaultContext.DB().Where("scraper_id = ?", scraperModel.ID).Delete(&models.ConfigItem{}).Error
 		Expect(err).NotTo(HaveOccurred(), "failed to delete config items")
 
